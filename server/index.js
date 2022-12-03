7f836485eb338d134224f5c3186c860c0e050bde
Pirin <chuckdanorris123@gmail.com>
2022-12-02 23:55:36 -0400

    first commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express "express";
+import mongoose "mongoose";
+import bodyParser "body-parser";
+import cors "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import multer "multer";
+import helmet "helmet";
+import path from "path";
+
