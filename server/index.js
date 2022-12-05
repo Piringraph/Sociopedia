8333f6848e59b18cf586ab1368b1316d9fae8db3
Pirin <chuckdanorris123@gmail.com>
2022-12-04 23:17:05 -0400

    added assets and auth update

--- server/index.js
+++ server/index.js
@@ -8,7 +8,11 @@ import multer from "multer";
 import helmet from "helmet";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./controllers/auth.js";
+import postRoutes  from "./routes.posts.js";
 import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
 
 /* CONFIGURATIONS */
 
@@ -39,6 +43,12 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post ("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/post", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
