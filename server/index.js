0690abde32576c7ede006dd976235c493094afe4
Pirin <chuckdanorris123@gmail.com>
2022-12-08 23:27:17 -0400

    added final features for backend

--- server/index.js
+++ server/index.js
@@ -9,11 +9,14 @@ import helmet from "helmet";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
-import userRoutes from "./controllers/auth.js";
-import postRoutes  from "./routes.posts.js";
+import userRoutes from "./routes/users.js";
+import postRoutes  from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts} from "./data/index.js"
 
 /* CONFIGURATIONS */
 
@@ -58,4 +61,8 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
 }).catch((error) => console.log(`${error} did not connect`));
