79ffe9d632cd076ddf7778d6b63b4f37bb5a3116
Pirin <chuckdanorris123@gmail.com>
2022-12-18 23:45:20 -0400

    added widget data

--- client/src/scenes/widgets/FriendListWidget.jsx
+++ client/src/scenes/widgets/FriendListWidget.jsx
@@ -8,7 +8,8 @@ import { setFriends } from "state";
 const FriendListWidget = ({ userId }) => {
     const dispatch = useDispatch();
     const { palette } = useTheme();
-    const token = useSelector((state) => state.user.friends);
+    const token = useSelector((state) => state.token);
+    const friends = useSelector((state) => state.user.friends);
 
     const getFriends = async () => {
         const response = await fetch(
